c7812c8574eb0d3568747407b8394f0acb0dc87d
obijuan <juan@iearobotics.com>
2019-06-02 12:27:31 +0200

    CT5: Example 1 added

---
+++ wiki/CT5-spi-esclavo/examples/01-SPI-LEDs/01-SPI-LEDs.ino
@@ -0,0 +1,32 @@
+#include <SPI.h>
+
+//-- Pin usado para la seleccion del esclavo
+#define SS 10
+
+void setup() {
+
+  //-- Inicializar SPI
+  SPI.begin();
+  SPI.beginTransaction (SPISettings (4000000, MSBFIRST, SPI_MODE0));
+
+}
+
+//-- Enviar un valor por el SPI para
+//-- sacarlo por los LEDs de la FPGA
+void write_LEDs(uint8_t value)
+{
+  digitalWrite(SS, LOW);
+  SPI.transfer(value); 
+  digitalWrite(SS, HIGH);
+}
+
+void loop() {
+
+  //-- Sacar valor 0xAA por los LEDs
+  write_LEDs(0xAA);
+  delay(500);
+
+  //-- Sacar valor 0x55 por los LEDs
+  write_LEDs(0x55);
+  delay(500);
+}
